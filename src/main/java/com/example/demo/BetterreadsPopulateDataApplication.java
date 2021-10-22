67e71a583849d939933998e29dec573e0cc42157
Subin C <schel@nets.eu>
2021-10-22 16:54:11 +0530

    Initial config

--- src/main/java/com/example/demo/BetterreadsPopulateDataApplication.java
+++ src/main/java/com/example/demo/BetterreadsPopulateDataApplication.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import connection.DataStaxAstraProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterreadsPopulateDataApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BetterreadsPopulateDataApplication.class, args);
 	}
+	
+	 /**
+     * This is necessary to have the Spring Boot app use the Astra secure bundle 
+     * to connect to the database
+     */
+	@Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+        Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
+    }
 
 }
